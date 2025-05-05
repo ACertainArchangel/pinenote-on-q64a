fb47d511da2c6e411e87c6588ca1f5e6c5920b40
hrdl <git@hrdl.eu>
2025-05-06 00:05:10 +0200

    scripts: minor fixes

--- bin/toggle_menu.sh
+++ bin/toggle_menu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nwg-menu -fm pcmanfm -term foot -va top -isl 64 -iss 64 -s /etc/sway_hrdl/nwg-panel/menu-start.css &
+nwg-menu -fm thunar -term kitty -va top -isl 64 -iss 64 -s /etc/sway_hrdl/nwg-panel/menu-start.css &
